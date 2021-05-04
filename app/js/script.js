535de549f9a4833a240a485787bdbd45bd612958
g1hong <g1hong8383@gmail.com>
2021-05-04 17:24:04 +0900

    버튼 누를 시 js 오류 발생 (resize 이슈) : 작업보류

--- app/js/script.js
+++ app/js/script.js
@@ -2,24 +2,79 @@ const information = document.querySelector(".information");
 const shareItems = document.querySelector(".share-items");
 
 const desktopShare = document.querySelector(".desktop-share");
-const shareIcon = document.querySelector(".share");
+// const shareIcon = document.querySelector(".share-icon");
 
-information.addEventListener("click", function () {
-  if (window.innerWidth < 768) {
-    information.style.display = "none";
-    shareItems.style.display = "flex";
+function resizeApply(width) {
+  //mobile;
+  if (width < 768) {
+    desktopShare.classList.remove("show");
+    information.classList.remove("show");
+    information
+      .querySelector(".share-icon")
+      .addEventListener("click", function () {
+        desktopShare.classList.remove("show");
+        information.classList.remove("show");
+        information.style.display = "none";
+        shareItems.style.display = "flex";
+      });
+    shareItems
+      .querySelector(".share-icon")
+      .addEventListener("click", function () {
+        shareItems.style.display = "none";
+        information.style.display = "flex";
+      });
   }
-});
-
-shareItems.addEventListener("click", function () {
-  if (window.innerWidth < 768) {
+  if (width >= 768) {
     shareItems.style.display = "none";
     information.style.display = "flex";
-  }
-});
+    information
+      .querySelector(".share-icon")
+      .addEventListener("click", function (e) {
+        shareItems.style.display = "none";
+        information.style.display = "flex";
 
-shareIcon.addEventListener("click", function (e) {
-  if (window.innerWidth > 767) {
-    desktopShare.classList.toggle("show");
+        if (desktopShare.classList.contains("show")) {
+          desktopShare.classList.remove("show");
+          information.classList.remove("show");
+        } else {
+          desktopShare.classList.toggle("show");
+          information.classList.toggle("show");
+        }
+      });
   }
-});
+}
+
+//     };
+// window.onload = function () {
+window.onresize = function () {
+  let width = window.innerWidth;
+  resizeApply(width);
+};
+window.onload = function () {
+  let width = window.innerWidth;
+  resizeApply(width);
+};
+// };
+
+// resizeApply();
+// 출처: https://extbrain.tistory.com/120 [확장형 뇌 저장소]
+// information.addEventListener("click", function () {
+//   if (window.innerWidth < 768) {
+//     information.style.display = "none";
+//     shareItems.style.display = "flex";
+//   }
+// });
+
+// shareItems.addEventListener("click", function () {
+//   if (window.innerWidth < 768) {
+//     shareItems.style.display = "none";
+//     information.style.display = "flex";
+//   }
+// });
+
+// shareIcon.addEventListener("click", function (e) {
+//   if (window.innerWidth > 767) {
+//     desktopShare.classList.toggle("show");
+//     information.classList.toggle("show");
+//   }
+// });
